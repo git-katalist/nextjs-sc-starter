7e946dbec308888ba0827571e21570f972531f76
git-katalist <79943903+git-katalist@users.noreply.github.com>
2022-12-14 06:23:38 +0000

    Deploying to gh-pages from @ git-katalist/nextjs-sc-starter@6266e2de66960194611fbb54e23ebccd87d668d3 🚀

---
+++ _next/static/chunks/pages/about-38ec8a76cbf7fe9d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{6515:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return u(9720)}])},9720:function(n,t,u){"use strict";u.r(t);var e=u(5893);let _=()=>(0,e.jsx)("div",{children:"We are all set to go"});t.default=_}},function(n){n.O(0,[774,888,179],function(){return n(n.s=6515)}),_N_E=n.O()}]);
