9179ee3c885a5f2f497f4ca82744a97cd6beb7ef
git-katalist <79943903+git-katalist@users.noreply.github.com>
2022-12-14 06:43:34 +0000

    Deploying to gh-pages from @ git-katalist/nextjs-sc-starter@4d7ed0336cf5598093f2532cbcd01c0f39502c24 🚀

---
+++ _next/static/nlE5TkRPcIJ6njSdGcOmz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-89b57580030c9955.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/about":["static/chunks/pages/about-54f421837ccb77c7.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/o1JBgXSDmdpGaUXGFsZhd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-282436b5e403e948.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/about":["static/chunks/pages/about-b29819a3dfffcdbf.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
